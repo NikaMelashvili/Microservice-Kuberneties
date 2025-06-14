094e6223db96f13fbe4e3959cd6ca533adbf94b6
NikaMelashvili <melashvilinikusha63@gmail.com>
2025-06-15 01:08:06 +0400

    Dockerized

--- auth-service/routes/auth.js
+++ auth-service/routes/auth.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const router = require('express').Router();
-const User = require('../models/user');
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
