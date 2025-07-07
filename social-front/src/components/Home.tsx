12806089351615ebc3af434df6da2949cd658fef
NikaMelashvili <melashvilinikusha63@gmail.com>
2025-07-07 15:34:55 +0400

    Readme update

--- social-front/src/components/Home.tsx
+++ social-front/src/components/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
 
   return (
     <div className='home-container'>
-      <h1>Chat App</h1>
+      <h1>Devops App</h1>
       <button onClick={() => navigate('/login')}>Login</button>
       <button onClick={() => navigate('/register')}>Register</button>
     </div>
